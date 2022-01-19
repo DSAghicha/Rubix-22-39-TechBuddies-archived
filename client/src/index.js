5d024d6771dee0374020ee46c12d0002e27f1ca6
Sapna Yadav <84910628+s-a-p-n-a@users.noreply.github.com>
2022-01-19 12:50:14 +0530

    feat: Added Home & Room routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home, Room } from './pages'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Routes>
+            <Route path='/' element={ <Home/> }/>
+            <Route path='/' element={ <Room/> }/>
+        </Routes>
+    </Router>,
+    document.getElementById('root')
+)
