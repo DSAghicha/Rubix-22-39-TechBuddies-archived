8fa92345e2b35c04c88aedd23931ddbeca97f7c8
DSAghicha <dsaghicha211@gmail.com>
2022-01-19 16:00:33 +0530

    build: Wrapping out App in Socket Context

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import { ContextProvider } from './SocketContext'
+
+// Pages
 import { Home, Room } from './pages'
 
 ReactDOM.render(
-    <Router>
+    <ContextProvider>
+        <Router>
         <Routes>
             <Route path='/' element={ <Home/> }/>
             <Route path='/room' element={ <Room/> }/>
         </Routes>
-    </Router>,
+    </Router>
+    </ContextProvider>,
     document.getElementById('root')
 )
